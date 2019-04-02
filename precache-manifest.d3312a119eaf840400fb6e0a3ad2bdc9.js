73f4e5dbc8fd69b895e334c78c2d2e418105befd
panos <panvarnavas@gmail.com>
2019-04-02 15:18:29 +0300

    Updates

---
+++ precache-manifest.d3312a119eaf840400fb6e0a3ad2bdc9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "67cc8ea1e4afd7ee2190",
+    "url": "/sha-react/static/js/runtime~main.104e5cfe.js"
+  },
+  {
+    "revision": "c1a83c480f027f578715",
+    "url": "/sha-react/static/js/main.3eaeab47.chunk.js"
+  },
+  {
+    "revision": "5694e1898ca4c92fd14d",
+    "url": "/sha-react/static/js/2.6408f43b.chunk.js"
+  },
+  {
+    "revision": "c1a83c480f027f578715",
+    "url": "/sha-react/static/css/main.90c59ebb.chunk.css"
+  },
+  {
+    "revision": "994fa2568bb4a1a2d4342c1a92b7b179",
+    "url": "/sha-react/index.html"
+  }
+];
